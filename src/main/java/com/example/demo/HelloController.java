0bd5bc91f7d1bcdc9745b47bfdb5c07f5b8993d9
Lotus <lotus@example.com>
2026-06-20 21:12:02 +0530

    Deploy version 2

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from GitOps Demo";
+        return "Version 2 deployed by GitOps";
     }
 }
